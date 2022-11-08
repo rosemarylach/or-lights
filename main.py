2bb5a48e6ca2e5687f40dfbb1678826a70f926f8
Rosemary Lach <rlach233@gmail.com>
2022-11-08 11:29:16 -0600

    add webcam video

--- main.py
+++ main.py
@@ -1,9 +1,19 @@
 from kivy.app import App
 from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.boxlayout import BoxLayout
+import time
 
 # root widget for .kv file
 class ZoilDefWidget(FloatLayout):
-    pass
+    def capture(self):
+        '''
+        Function to capture the images and give them the names
+        according to their captured time and date.
+        '''
+        camera = self.ids['camera']
+        timestr = time.strftime("%Y%m%d_%H%M%S")
+        camera.export_to_png("IMG_{}.png".format(timestr))
+        print("Captured")
 
 class ZoilDefApp(App):
     title = "ZoilDef App"
