bc68efd5afc6a730a51dc0a466dbe5012dffb399
JUstin Guilak <justinguilak@gmail.com>
2022-11-16 11:48:08 -0600

    preliminary stepper testing using online library (it works!)

--- light-control/light-control/light-control.ino
+++ light-control/light-control/light-control.ino
@@ -1,9 +1,30 @@
-void setup() {
-  // put your setup code here, to run once:
+// Include the AccelStepper Library
+#include <AccelStepper.h>
 
+// Define pin connections
+const int dirPin = 50;
+const int stepPin = 52;
+
+// Define motor interface type
+#define motorInterfaceType 1
+
+// Creates an instance
+AccelStepper myStepper(motorInterfaceType, stepPin, dirPin);
+
+void setup() {
+  // set the maximum speed, acceleration factor,
+  // initial speed and the target position
+  myStepper.setMaxSpeed(1000);
+  myStepper.setAcceleration(100);
+  myStepper.setSpeed(1000);
+  myStepper.moveTo(2000);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  // Change direction once the motor reaches target position
+  if (myStepper.distanceToGo() == 0) 
+    myStepper.moveTo(-myStepper.currentPosition());
 
+  // Move the motor one step
+  myStepper.run();
 }
