1a4d15da3dc1d8c161e8e10a350a5090cea302dc
JUstin Guilak <justinguilak@gmail.com>
2022-11-18 12:34:14 -0600

    added user control of stepper via serial monitor

--- light-control/light-control/light-control.ino
+++ light-control/light-control/light-control.ino
@@ -5,11 +5,18 @@
 const int dirPin = 50;
 const int stepPin = 52;
 
+float inputRevs = 0;
+String input;
+
+#define STEPS_PER_REV 200
+
+
 // Define motor interface type
 #define motorInterfaceType 1
 
 // Creates an instance
 AccelStepper myStepper(motorInterfaceType, stepPin, dirPin);
+
 
 void setup() {
   // set the maximum speed, acceleration factor,
@@ -17,14 +24,26 @@ void setup() {
   myStepper.setMaxSpeed(1000);
   myStepper.setAcceleration(100);
   myStepper.setSpeed(1000);
-  myStepper.moveTo(2000);
+  Serial.begin(9600);
 }
 
 void loop() {
   // Change direction once the motor reaches target position
-  if (myStepper.distanceToGo() == 0) 
-    myStepper.moveTo(-myStepper.currentPosition());
-
-  // Move the motor one step
+  if(Serial.available() && inputRevs == 0){
+    inputRevs = Serial.parseFloat();
+    //Serial.println(inputRevs);
+    //inputRevs = atof(input);
+    //Serial.println(myStepper.currentPosition());
+    //Serial.println(STEPS_PER_REV * inputRevs);
+    myStepper.moveTo(myStepper.currentPosition() + STEPS_PER_REV * inputRevs);
+  }
+  if (myStepper.distanceToGo() == 0 && inputRevs != 0) {
+      Serial.print("Moved ");
+      Serial.print(inputRevs);
+      Serial.println(" revolutions.");
+      inputRevs = 0;
+      Serial.println("How many revolutions to turn?");
+    }
+    
   myStepper.run();
 }
